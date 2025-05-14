bfec51a465cc6f55531a03fedd250034d231b855
Rashim-Sunar <rashimsunar.02@gmail.com>
2025-05-13 21:49:36 -0400

    Network response like success, error, loading handled

--- app/src/main/java/com/example/weatherapp/WeatherPage.kt
+++ app/src/main/java/com/example/weatherapp/WeatherPage.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
@@ -21,10 +23,14 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.weatherapp.api.NetworkResponse
 
 @Composable
 fun WeatherPage(weatherViewModel: WeatherViewModel){
     var city by remember{ mutableStateOf(" ") }
+
+    val weatherResult = weatherViewModel.weatherResult.collectAsState(NetworkResponse.Loading)
+
     Column (
         modifier = Modifier.fillMaxWidth().padding(8.dp),
         horizontalAlignment = Alignment.CenterHorizontally
@@ -49,5 +55,16 @@ fun WeatherPage(weatherViewModel: WeatherViewModel){
             }
         }
 
+        when(val result = weatherResult.value){
+            is NetworkResponse.Error -> {
+                Text(text = result.message)
+            }
+            NetworkResponse.Loading -> {
+                CircularProgressIndicator()
+            }
+            is NetworkResponse.Success<*> -> {
+               Text(text = result.data.toString())
+            }
+        }
     }
 }
--- app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
+++ app/src/main/java/com/example/weatherapp/WeatherViewModel.kt
@@ -3,20 +3,37 @@ package com.example.weatherapp
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.weatherapp.api.NetworkResponse
 import com.example.weatherapp.api.RetrofitInstance
+import com.example.weatherapp.api.WeatherModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
 class WeatherViewModel : ViewModel(){
 
     private val weatherApi = RetrofitInstance.weatherApi
+    private val _weatherResult = MutableStateFlow<NetworkResponse<WeatherModel>>(
+        value = NetworkResponse.Loading
+    )
+    val weatherResult : Flow<NetworkResponse<WeatherModel>> get() = _weatherResult
+
 
     fun getData(city: String){
        viewModelScope.launch {
-           val response = weatherApi.getWeather(apiKey = Constant.apiKey, city = city)
-           if(response.isSuccessful){
-               Log.d("Response", response.body().toString())
-           }else{
-               Log.d("Error", response.message())
+           try{
+               val response = weatherApi.getWeather(apiKey = Constant.apiKey, city = city)
+               if(response.isSuccessful){
+                   //Log.d("Response", response.body().toString())
+                   response.body()?.let{
+                       _weatherResult.value = NetworkResponse.Success(it)
+                   }
+               }else{
+                   //Log.d("Error", response.message())
+                   _weatherResult.value = NetworkResponse.Error("Failed to load data")
+               }
+           }catch(e : Exception){
+               _weatherResult.value = NetworkResponse.Error("Failed to load data")
            }
        }
     }
